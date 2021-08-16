fc22841d1b7047c729cea16ddbf774c2b2f92580
Bilal Khundmiri <khundmirib@gmail.com>
2021-08-16 01:36:29 -0400

    added delete comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,7 +1,9 @@
 const Post = require('../../models/Post')
 const checkAuth = require("../../Utilities/check-auth")
 
+const { AuthenticationError } = require('apollo-server')
 const { UserInputError } = require('apollo-server')
+
 
 module.exports = {
     Mutation: {
@@ -26,6 +28,25 @@ module.exports = {
                 await post.save()
                 return post
             } else throw new UserInputError('Post not found')
+        },
+        async deleteComment(_, { postId, commentId }, context){
+            const { username } = checkAuth(context)
+
+            const post = await Post.findById(postId)
+
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId)
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentIndex, 1)
+                    await post.save()
+
+                    return post
+                } else {
+                    throw new AuthenticationError('Action Not Allowed')
+                }
+            } else {
+                throw new UserInputError('Post not found')
+            }
         }
     }
 }
